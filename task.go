9b34e28b4146954c3fec556fde15aa8d663ea4cf
Aleksandr <strtmg@yandex.ru>
2021-04-22 23:21:27 +0700

    Extend task abstraction

--- task.go
+++ task.go
@@ -1,5 +1,11 @@
 package main
 
+// Task Representation of the task for execution.
 type Task interface {
+
+	// Result Returns result of the execution.
 	Result() interface{}
+
+	// Error Returns error what is happened during execution.
+	Error() error
 }
